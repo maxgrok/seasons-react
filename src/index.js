d312ebc5582d79b533834cf8ba56a472c2f3b5a4
Max Goodman <max.goodman@flatironschool.com>
2019-03-06 12:37:31 -0500

    adding logic for summer and winter

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-
+import SeasonDisplay from './SeasonDisplay';
 class App extends Component{
     state = {
         lat: null, 
         errorMessage: ""
     }
+
     componentDidMount(){
         console.log("My component was rendered to the screen")
         window.navigator.geolocation.getCurrentPosition(
@@ -27,7 +28,7 @@ class App extends Component{
             )
         }
         if (!this.state.errorMessage && this.state.lat){
-            return <div>Latitude: {this.state.lat}</div>
+            return <SeasonDisplay lat={this.state.lat} />
         }
 
         return <div>Loading...</div>
